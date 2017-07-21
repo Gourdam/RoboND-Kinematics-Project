a135d2dc6f42450444512964ebe76588eb3c9596
Jordan Hu <hugordam@gmail.com>
2017-07-21 17:37:40 -0400

    trying custom thetas

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -193,6 +193,10 @@ def handle_calculate_IK(req):
             theta4 += pi
             if(theta4 > 2*pi):
                 theta4 -= 2*pi
+            if(theta5 > 2*pi):
+                theta5 -= 2*pi
+            if(theta6 > 2*pi):
+                theta6 -= 2*pi
             print theta4, theta5, theta6
 
             # r23 = R3_6[1, 2]
