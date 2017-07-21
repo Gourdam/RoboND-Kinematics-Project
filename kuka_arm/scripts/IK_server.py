7d643f0b0765449492ae18abdbeafda9029251dc
Jordan Hu <hugordam@gmail.com>
2017-07-21 18:28:22 -0400

    trying custom thetas

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -190,6 +190,7 @@ def handle_calculate_IK(req):
             R3_6_converted = matrix2numpy(R3_6)
 
             theta4 += 2*pi
+            theta6 +- pi
 
             theta4, theta5, theta6 = tf.transformations.euler_from_matrix(R3_6_converted)
             print theta4, theta5, theta6
