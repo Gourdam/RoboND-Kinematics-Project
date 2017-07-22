f1cb421d00a64548bab5b4d3131236959bb5a58b
Jordan Hu <hugordam@gmail.com>
2017-07-22 16:30:17 -0400

    added correction rotations

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -196,12 +196,12 @@ def handle_calculate_IK(req):
             elif theta4 < -2*pi:
                 theta4 += 2*pi
 
-            # theta5 += pi/2
-            # if theta5 > 2*pi:
-            #     theta5 -= 2*pi
-            # elif theta5 < -2*pi:
-            #     theta5 += 2*pi
-            # print theta4, theta5, theta6
+            theta5 -= pi/2
+            if theta5 > 2*pi:
+                theta5 -= 2*pi
+            elif theta5 < -2*pi:
+                theta5 += 2*pi
+            print theta4, theta5, theta6
 
             # r23 = R3_6[1, 2]
             # r33 = R3_6[2, 2]
